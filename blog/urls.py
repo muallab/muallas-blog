fac4397f75f9ed56c08236106306ef883fc95efe
mualla <mabursal@gmail.com>
2025-06-18 16:31:19 -0400

    Add Article model, admin, list view, URL & template

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',       views.homepage, name='homepage'),
     path('about/', views.about,    name='about'),
+    path('articles/',views.article_list, name='article_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ def homepage(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+from .models import Article
+
+def article_list(request):
+    articles = Article.objects.order_by('-created_at')
+    return render(request, 'blog/article_list.html', {'articles': articles})
