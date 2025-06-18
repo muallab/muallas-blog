70f43268099894031986dba47298b321749c2874
mualla <mabursal@gmail.com>
2025-06-18 17:11:31 -0400

    Register Article model with the Django admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Article
 admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display  = ('title', 'created_at')
+    list_filter   = ('created_at',)
+    search_fields = ('title', 'content')
